292e6aa25c8eded0f72904833cbd0d1bd2613af9
AValerievna <a.valerievna12@gmail.com>
2019-01-24 20:38:43 +0300

    drafts

---
+++ RobotTask/Libraries/APIWork.py
@@ -0,0 +1,27 @@
+import base64
+
+import requests
+
+
+class APIWork(object):
+    BASE_URL = "http://httpbin.org/"
+    GET = "get"
+    STREAM = "stream/%d"
+    BASIC_AUTH = "basic-auth/%s/%s"
+    COLON = ":"
+    AUTHORIZATION = "Authorization"
+    BASIC = "Basic ";
+
+    def __init__(self):
+        self._base_url = self.BASE_URL
+
+    def request_get(self):
+        return requests.get(self._base_url + self.GET)
+
+    def request_stream(self, number):
+        return requests.get(self._base_url + self.STREAM % number)
+
+    def request_basic_auth(self, valid_usr, valid_pswd, actaul_usr, actual_pswd):
+        authHeader = base64.b64encode(bytes(actaul_usr + self.COLON + actual_pswd, 'utf-8'))
+        return requests.get(self._base_url + self.BASIC_AUTH % (valid_usr, valid_pswd), headers=
+        {self.AUTHORIZATION: self.BASIC + authHeader})
